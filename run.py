460dd951a46d34b03ac05ffce59ebc07a3b47667
vkudak <vkudak@ukr.net>
2022-11-08 19:03:14 +0200

    Change Flask default development server options

--- run.py
+++ run.py
@@ -10,8 +10,7 @@ log.setLevel(logging.INFO)
 if __name__ == "__main__":
     # from app.models import db
     # db.create_all()
-    # app.run(host="0.0.0.0", port=5000, debug=True)
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000, debug=True)
 else:
     # https://trstringer.com/logging-flask-gunicorn-the-manageable-way/
     # https://stackoverflow.com/questions/26578733/why-is-flask-application-not-creating-any-logs-when-hosted-by-gunicorn
